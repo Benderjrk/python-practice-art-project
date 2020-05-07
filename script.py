8d12f0c8a7bcd5d9201d76864ea6210bd0c48d54
Justin Bender <jrkbender@gmail.com>
2020-05-07 12:26:45 -0400

    creating art listings with clients and art class

--- script.py
+++ script.py
@@ -19,6 +19,11 @@ class Art:
         name = self.owner.name,
         location = self.owner.location
       )
+  def change_owner(self, new_owner):
+    if self.owner != new_owner:
+      self.owner = new_owner
+    else:
+      print("They already own this art")
 
 class MarketPlace:
   def __init__(self):
@@ -50,10 +55,45 @@ class Client:
     else:
       self.location = location
 
+  def sell_artwork(self, artwork, price):
+    if artwork.owner.name == self.name:
+      price = "${} USD".format(price)
+      new_listing = Listing(artwork, price, self)
+      veneer.add_listing(new_listing)
+  def buy_artwork(self, artwork):
+    if artwork.owner.name != self.name:
+      for listing in veneer.listings:
+        if artwork.title == listing.art.title:
+          art_listing = listing
+          # change owner
+          artwork.change_owner(self)
+          # remove listing
+          veneer.remove_listing(art_listing)
+    else:
+      print("You already own this art piece.")
+
 edytta = Client("Edytta Halpirt", False)
 moma = Client("The MOMA", True, "New York")
 
 girl_with_mandolin = Art("Picasso, Pablo", "Girl with a Mandolin (Fanny Tellier)", "oil on canvas", 1910, edytta)
 
+# print(girl_with_mandolin)
+
+class Listing:
+  def __init__(self, art, price, seller):
+    self.art = art
+    self.price = price
+    self.seller = seller
+  def __repr__(self):
+    return '"{name}": {price}'.format(name=self.art.title,price=self.price)
+
+edytta.sell_artwork(girl_with_mandolin, 6000000)
+
+# veneer.show_listings()
+# print(veneer.listings)
+
+moma.buy_artwork(girl_with_mandolin)
+
 print(girl_with_mandolin)
 
+veneer.show_listings()
